bc50e04e1c2bd3b190a1ac798f09b4588291e10e
Xilence997 <artemyevart@gmail.com>
2024-08-23 16:42:22 +0300

    Added meta tags

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -78,7 +78,7 @@ function AboutMe() {
                   </a>
                 </li>
                 <li>
-                  <a class="group flex items-center py-3 hidden" href="#projects">
+                  <a class="group flex items-center py-3" href="#projects">
                     <span class="nav-indicator mr-4 h-px w-8 bg-black transition-all group-hover:w-16 group-hover:bg-orange group-focus-visible:w-16 group-focus-visible:bg-orange motion-reduce:transition-none"></span>
                     <span class="nav-text text-xs font-bold uppercase tracking-widest text-black group-hover:text-orange group-focus-visible:text-orange">
                       Projects
@@ -274,6 +274,80 @@ function AboutMe() {
                         <li class="mr-1.5 mt-2">
                           <div class="flex items-center rounded-full bg-orange/25 px-3 py-1 text-xs font-medium leading-5 text-orange">
                             Klaviyo
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+                <li class="mb-12">
+                  <div class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                    <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-2xl transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:border-orange/0 lg:group-hover:border-orange/50 lg:group-hover:border lg:group-hover:bg-[#1E1E1E] lg:group-hover:drop-shadow-lg"></div>
+                    <header
+                      class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-text font-header sm:col-span-2"
+                      aria-label="2018 to 2024"
+                    >
+                      2020 — 2021
+                    </header>
+                    <div class="z-10 sm:col-span-6">
+                      <h3 class="font-medium font-header leading-snug text-white">
+                        <div>
+                          <a
+                            class="inline-flex items-baseline font-medium leading-tight text-white hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                            href="https://www.fiverr.com/artemeister/design-a-stream-overlay-package-for-you"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            aria-label="Digital Marketing Specialist at Park Inn (opens in a new tab)"
+                          >
+                            <span class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                            <span>
+                              Freelance Graphic Designer ·{" "}
+                              <span class="inline-block">
+                                Fiverr
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                  class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    fill-rule="evenodd"
+                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                    clip-rule="evenodd"
+                                  ></path>
+                                </svg>
+                              </span>
+                            </span>
+                          </a>
+                        </div>
+                      </h3>
+                      <p class="mt-2 text-sm leading-normal">
+                        Managed the updating and oversight of social media and
+                        all web-based platforms. Created and designed graphics
+                        for upcoming events, social media updates, posters, and
+                        mailings. Handled the entry and organization of customer
+                        data for future sales or mailing campaigns. Handled the
+                        processing and organization of customer data for future sales
+                        or mailing campaigns.
+                      </p>
+                      <ul
+                        class="mt-2 flex flex-wrap"
+                        aria-label="Technologies used"
+                      >
+                        <li class="mr-1.5 mt-2">
+                          <div class="flex items-center rounded-full bg-orange/25 px-3 py-1 text-xs font-medium leading-5 text-orange">
+                            Adobe Photoshop
+                          </div>
+                        </li>
+                        <li class="mr-1.5 mt-2">
+                          <div class="flex items-center rounded-full bg-orange/25 px-3 py-1 text-xs font-medium leading-5 text-orange">
+                            Adobe Illustrator
+                          </div>
+                        </li>
+                        <li class="mr-1.5 mt-2">
+                          <div class="flex items-center rounded-full bg-orange/25 px-3 py-1 text-xs font-medium leading-5 text-orange">
+                            Adobe After Effects
                           </div>
                         </li>
                       </ul>
